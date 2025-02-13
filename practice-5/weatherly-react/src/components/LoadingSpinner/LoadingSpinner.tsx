72e5c3798d40ae522c940131c546dbd43f0e4bfd
dmanea <dmanea@adobe.com>
2025-02-13 02:11:36 +0200

    Add delay before prompting the user to reconnect to the Internet in `LoadingSpinner`

--- practice-5/weatherly-react/src/components/LoadingSpinner/LoadingSpinner.tsx
+++ practice-5/weatherly-react/src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,10 +1,23 @@
+import { useState, useEffect } from "react";
 import styles from "./LoadingSpinner.module.css";
 
 function LoadingSpinner() {
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    // Set a timeout to display the message after 5 seconds
+    const timer = setTimeout(() => {
+      setShowMessage(true);
+    }, 5000);
+
+    // Cleanup the timer if the component is unmounted
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className={styles.loadingSpinner}>
       <div className={styles.spinnerCircle}></div>
-      <p>Please reconnect to the Internet.</p>
+      {showMessage && <p>Please reconnect to the Internet.</p>}
     </div>
   );
 }
